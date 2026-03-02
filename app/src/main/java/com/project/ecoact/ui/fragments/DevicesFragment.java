39df667bfbaccbc738cb3ba4a23add7ea9bb7626
Ton Nom <ton.email@example.com>
2026-03-02 17:23:41 +0100

    recommit

---
+++ app/src/main/java/com/project/ecoact/ui/fragments/DevicesFragment.java
@@ -0,0 +1,17 @@
+package com.project.ecoact.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+import com.project.ecoact.R;
+
+public class DevicesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_devices, container, false);
+    }
+}
---
+++ app/src/main/java/com/project/ecoact/ui/fragments/MainActivity.java
@@ -0,0 +1,22 @@
+package com.project.ecoact.ui.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Récupérer le NavHostFragment
+//        NavHostFragment navHostFragment =
+//                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+//
+//        if (navHostFragment != null) {
+//            NavController navController = navHostFragment.getNavController();
+//            BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+//            NavigationUI.setupWithNavController(bottomNav, navController);
+//        }
+//    }
+}
