9dbc8e787b4f5304dab97f223e6dee2659afcc1c
03cynthi <147426312+03cynthi@users.noreply.github.com>
2026-04-29 16:28:38 +0200

    ajout page accueil

---
+++ app/src/main/java/com/project/ecoact/data/EnergyDao.java
@@ -0,0 +1,18 @@
+package com.project.ecoact.data;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface EnergyDao {
+    @Insert
+    void insert(EnergyEntity data);
+
+    @Query("SELECT * FROM energy_data WHERE type = :type AND period = :period")
+    EnergyEntity getByTypeAndPeriod(String type, String period);
+
+    @Query("SELECT * FROM energy_data WHERE period = :period")
+    List<EnergyEntity> getAllByPeriod(String period);
+}
